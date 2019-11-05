11bcb8b881c0b822f62655518d45b80e9fc4f517
Akos Kitta <kittaakos@typefox.io>
2019-11-05 14:30:13 +0100

    GH-11: Adjusted the lookup to the latest VS Code.
     - [win32]: Use `which` when cannot find Git on the `$PATH`.
     - Listen on the process' `close` instead of `exit`.
     - Moved the Windows build to Travis.
     - Pinned the versions with `yarn.lock`.
     - Bumped up the dependencies.
     - New version -> `0.0.2`.
    
    Closes #11.
    
    Co-authored-by: Alex Tugarev <alex.tugarev@typefox.io>
    Co-authored-by: Akos Kitta <kittaakos@typefox.io>
    
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- src/find-git-exec.ts
+++ src/find-git-exec.ts
@@ -3,113 +3,51 @@
  * See the LICENSE file in the project root for license information.
  */
 
-import * as fs from 'fs';
+// The current implementation is based on https://github.com/microsoft/vscode/blob/master/extensions/git/src/git.ts#L50-L141
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import * as path from 'path';
+import * as which from 'which';
 import * as cp from 'child_process';
 
 /**
- * Bare minimum information of the locally available git executable.
+ * Bare minimum information of the locally available Git executable.
  */
 export interface Git {
 
     /**
      * The FS path to the Git executable.
      */
-    readonly path: string,
+    readonly path: string;
     /**
      * The Git version. [`git --version`]
      */
-    readonly version: string,
+    readonly version: string;
     /**
      * The path to wherever your core Git programs are installed. [`git --exec-path`]
      */
-    readonly execPath: string
+    readonly execPath: string;
 
 }
 
 /**
  * Resolves to the path of the locally available Git executable. Will be rejected if Git cannot be found on the system.
+ * `hint` can be provided as the initial lookup path, and `onLookup` function is used for logging during the Git discovery.
  */
-export default function (): Promise<Git> {
-    switch (process.platform) {
-        case 'win32': return findGitWin32();
-        default: return findGitNix();
-    }
-}
-
-async function findGit(path: string): Promise<Git> {
-    return new Promise<Git>(async (resolve, reject) => {
-        try {
-            resolve({
-                path,
-                version: parseVersion(await exec(path, '--version')),
-                execPath: normalizePath(await exec(path, '--exec-path'))
-            });
-        } catch (error) {
-            reject(error);
-        }
-    });
-}
-
-async function exec(path: string, command: string | string[]): Promise<string> {
-    return new Promise<string>((resolve, reject) => {
-        const buffers: Buffer[] = [];
-        const child = cp.spawn(path, Array.isArray(command) ? command : [command]);
-        child.stdout.on('data', (b: Buffer) => buffers.push(b));
-        child.on('error', reject);
-        child.on('exit', code => code ? reject(new Error(`Git not found under '${path}'.`)) : resolve(toUtf8String(buffers)));
-    });
-}
-
-async function findGitNix(): Promise<Git> {
-    return new Promise<Git>((resolve, reject) => {
-        cp.exec('which git', (error, buffer) => {
-            if (error) {
-                return reject('Git not found.');
+export default async function ({ hint, onLookup }: { hint: string | undefined, onLookup: (path: string) => void } = { hint: undefined, onLookup: () => { } }): Promise<Git> {
+    const first = hint ? findSpecificGit(hint, onLookup) : Promise.reject<Git>(null);
+    return first
+        .then(undefined, () => {
+            switch (process.platform) {
+                case 'darwin': return findGitDarwin(onLookup);
+                case 'win32': return findGitWin32(onLookup);
+                default: return which('git').then(gitOnPath => findSpecificGit(gitOnPath, onLookup));
             }
-            const path = buffer.toString().replace(/^\s+|\s+$/g, '');
-            if (path !== 'usr/bin/git' || process.platform !== 'darwin') {
-                return resolve(findGit(path));
-            }
-            cp.exec('xcode-select -p', (error: any) => {
-                if (error && error.code === 2) {
-                    return reject(new Error('Git not found.'));
-                }
-                resolve(findGit(path));
-            });
-        });
-    });
-}
-
-async function findSystemGitWin32(base: string): Promise<Git> {
-    if (!base) {
-        throw new Error(`Git not found.`);
-    }
-    return findGit(path.join(base, 'Git', 'cmd', 'git.exe'));
-}
-
-async function findGitHubGitWin32(): Promise<Git> {
-    const github = path.join(env('LOCALAPPDATA'), 'GitHub');
-    return new Promise<Git>((resolve, reject) => {
-        fs.readdir(github, (error: NodeJS.ErrnoException, files: string[]) => {
-            if (error) {
-                return reject(error);
-            }
-            const git = files.filter(file => /^PortableGit/.test(file)).shift();
-            if (!git) {
-                return reject(new Error(`Git not found.`));
-            }
-            resolve(findGit(path.join(github, git, 'cmd', 'git.exe')));
         })
-    });
-}
-
-async function findGitWin32(): Promise<Git> {
-    return findSystemGitWin32(env('ProgramW6432'))
-        .then(undefined, () => findSystemGitWin32(env('ProgramFiles(x86)'))
-            .then(undefined, () => findSystemGitWin32(env('ProgramFiles'))
-                .then(undefined, () => findGit('git'))
-                .then(undefined, () => findGitHubGitWin32())));
+        .then(null, () => Promise.reject(new Error('Git installation not found.')));
 }
 
 function toUtf8String(buffers: Buffer[]): string {
@@ -124,6 +62,98 @@ function normalizePath(pathToNormalize: string): string {
     return path.normalize(pathToNormalize);
 }
 
-function env(key: string): string {
-    return process.env[key] || '';
+function findSpecificGit(path: string, onLookup: (path: string) => void): Promise<Git> {
+    return new Promise<Git>((c, e) => {
+        onLookup(path);
+        exec(path, '--version').then(version => {
+            exec(path, '--exec-path').then(execPath => {
+                c({ path, version: parseVersion(version), execPath: normalizePath(execPath) });
+            });
+        }).catch(err => {
+            e(err);
+        });
+    });
+}
+
+async function exec(path: string, command: string | string[]): Promise<string> {
+    return new Promise<string>((c, e) => {
+        const buffers: Buffer[] = [];
+        const child = cp.spawn(path, Array.isArray(command) ? command : [command]);
+        child.stdout.on('data', (b: Buffer) => buffers.push(b));
+        child.on('error', e);
+        child.on('close', code => code ? e(new Error(`Git not found under '${path}'.`)) : c(toUtf8String(buffers)));
+    });
+}
+
+function findGitDarwin(onLookup: (path: string) => void): Promise<Git> {
+    return new Promise<Git>((c, e) => {
+        cp.exec('which git', (err, gitPathBuffer) => {
+            if (err) {
+                return e('git not found');
+            }
+
+            const path = gitPathBuffer.toString().replace(/^\s+|\s+$/g, '');
+
+            function getVersion(path: string) {
+                onLookup(path);
+
+                // make sure git executes
+                cp.exec('git --version', (err, stdout) => {
+
+                    if (err) {
+                        return e('git not found');
+                    }
+
+                    const version = parseVersion(stdout.trim());
+                    cp.exec('git --exec-path', (err, stdout) => {
+
+                        if (err) {
+                            return e('git not found');
+                        }
+
+                        const execPath = normalizePath(stdout.trim());
+                        return c({ path, version, execPath });
+                    });
+
+                });
+            }
+
+            if (path !== '/usr/bin/git') {
+                return getVersion(path);
+            }
+
+            // must check if XCode is installed
+            cp.exec('xcode-select -p', (err: any) => {
+                if (err && err.code === 2) {
+                    // git is not installed, and launching /usr/bin/git
+                    // will prompt the user to install it
+
+                    return e('git not found');
+                }
+
+                getVersion(path);
+            });
+        });
+    });
+}
+
+function findSystemGitWin32(base: string, onLookup: (path: string) => void): Promise<Git> {
+    if (!base) {
+        return Promise.reject<Git>('Not found');
+    }
+
+    return findSpecificGit(path.join(base, 'Git', 'cmd', 'git.exe'), onLookup);
+}
+
+async function findGitWin32InPath(onLookup: (path: string) => void): Promise<Git> {
+    const whichPromise = new Promise<string>((c, e) => which('git.exe', (err, path) => err ? e(err) : c(path)));
+    return whichPromise.then(path => findSpecificGit(path, onLookup));
+}
+
+async function findGitWin32(onLookup: (path: string) => void): Promise<Git> {
+    return findSystemGitWin32(process.env['ProgramW6432'] as string, onLookup)
+        .then(undefined, () => findSystemGitWin32(process.env['ProgramFiles(x86)'] as string, onLookup))
+        .then(undefined, () => findSystemGitWin32(process.env['ProgramFiles'] as string, onLookup))
+        .then(undefined, () => findSystemGitWin32(path.join(process.env['LocalAppData'] as string, 'Programs'), onLookup))
+        .then(undefined, () => findGitWin32InPath(onLookup));
 }
